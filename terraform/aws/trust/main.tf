d21d48f7851dd23703c6b4898a02feecf89a124e
kamiKAZIK <eduardas.kazakas@gmail.com>
2024-07-14 22:39:36 +0300

    WIP

--- terraform/aws/trust/main.tf
+++ terraform/aws/trust/main.tf
@@ -8,56 +8,65 @@ data "tls_certificate" "tfc_certificate" {
 
 resource "aws_iam_openid_connect_provider" "tfc_provider" {
   url             = data.tls_certificate.tfc_certificate.url
-  client_id_list  = [var.tfc_aws_audience]
-  thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
+  client_id_list  = [
+    var.tfc_aws_audience
+  ]
+  thumbprint_list = [
+    data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint
+  ]
+}
+
+data "aws_iam_policy_document" "tfc_role" {
+  statement {
+    condition {
+      test     = "StringEquals"
+      variable = "${var.tfc_hostname}:aud"
+      values = [
+        "${one(aws_iam_openid_connect_provider.tfc_provider.client_id_list)}"
+      ]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "${var.tfc_hostname}:sub"
+      values = [
+        "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:*"
+      ]
+    }
+    effect    = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [
+        aws_iam_openid_connect_provider.tfc_provider.arn
+      ]
+    }
+    actions   = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+  }
 }
 
 resource "aws_iam_role" "tfc_role" {
   name = "tfc-role"
-
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Effect": "Allow",
-     "Principal": {
-       "Federated": "${aws_iam_openid_connect_provider.tfc_provider.arn}"
-     },
-     "Action": "sts:AssumeRoleWithWebIdentity",
-     "Condition": {
-       "StringEquals": {
-         "${var.tfc_hostname}:aud": "${one(aws_iam_openid_connect_provider.tfc_provider.client_id_list)}"
-       },
-       "StringLike": {
-         "${var.tfc_hostname}:sub": "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:*"
-       }
-     }
-   }
- ]
+  assume_role_policy = data.aws_iam_policy_document.tfc_role.json
 }
-EOF
+
+data "aws_iam_policy_document" "tfc_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+       "s3:*",
+       "iot:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
 
 resource "aws_iam_policy" "tfc_policy" {
   name        = "tfc-policy"
   description = "TFC run policy"
-
-  policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Effect": "Allow",
-     "Action": [
-       "s3:*",
-       "iot:*"
-     ],
-     "Resource": "*"
-   }
- ]
-}
-EOF
+  policy = data.aws_iam_policy_document.tfc_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "tfc_policy_attachment" {
