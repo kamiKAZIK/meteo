71d2066bf6ed1fb78bffea0fc28f949c82585fbd
kamiKAZIK <eduardas.kazakas@gmail.com>
2024-07-14 17:08:15 +0300

    WIP

--- terraform/aws/infra/outputs.tf
+++ terraform/aws/infra/outputs.tf
@@ -1,4 +1,4 @@
-output "meteo_data_name" {
+output "meteo_data_arn" {
   description = "S3 bucket name for Meteo data storage"
-  value       = aws_s3_bucket.meteo_data.name
+  value       = aws_s3_bucket.meteo_data.arn
 }
