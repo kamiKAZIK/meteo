69a81fef9bdf950f41940964ea124a71fed7b246
kamiKAZIK <eduardas.kazakas@gmail.com>
2024-07-14 17:01:16 +0300

    WIP

--- terraform/prod/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "aparatus-timeseries-data-prod" {
-  bucket = "eucentral1-aparatus-timeseries-data-prod"
-
-  tags = {
-    Name        = "Aparatus timeseries data bucket for PROD"
-    Environment = "PROD"
-  }
-}
