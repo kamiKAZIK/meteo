a2501d760e2c76bf84aedd8f03463e5dbc6d39ac
kamiKAZIK <eduardas.kazakas@gmail.com>
2024-08-10 13:50:52 +0300

    wip

--- terraform/aws/infra/iam.tf
+++ terraform/aws/infra/iam.tf
@@ -51,12 +51,22 @@ resource "aws_iam_role" "meteo_iam_role" {
   assume_role_policy = data.aws_iam_policy_document.meteo_assume_role_policy_document.json
 }
 
+resource "aws_iam_policy" "meteo_s3_policy" {
+  name        = "meteo-s3"
+  policy      = data.meteo_s3_policy_document.policy.json
+}
+
+resource "aws_iam_policy" "meteo_glue_policy" {
+  name        = "meteo-glue"
+  policy      = data.meteo_glue_policy_document.policy.json
+}
+
 resource "aws_iam_role_policy_attachment" "meteo_s3_policy_attachment" {
-  role   = aws_iam_role.meteo_iam_role.name
-  policy = aws_iam_policy.meteo_s3_policy_document.json
+  role       = aws_iam_role.meteo_iam_role.name
+  policy_arn = aws_iam_policy.meteo_s3_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "meteo_glue_policy_attachment" {
-  role   = aws_iam_role.meteo_iam_role.name
-  policy = aws_iam_policy.meteo_glue_policy_document.json
+  role       = aws_iam_role.meteo_iam_role.name
+  policy_arn = aws_iam_policy.meteo_glue_policy.arn
 }
